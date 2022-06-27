727b5411eed7218a55408a5c01fc5aab7ed3a3f0
Frank Bucalo <frankiebucalo@gmail.com>
2022-06-27 07:23:31 -0400

    update charts.js

--- js/charts.js
+++ js/charts.js
@@ -74,7 +74,7 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otu_ids.slice(0,10).map(ids => `OTU `{ids}).reverse();
+    var yticks = otu_ids.slice(0,10).map(ids => `OTU ${ids}`).reverse();
 
     // 8. Create the trace for the bar chart. 
     var barData = [{
